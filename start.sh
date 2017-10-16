2476cb5e94a90770dabf4edf2fc8c1ce78ccd0d4
Eric Paris <eparis@redhat.com>
2017-10-16 10:46:55 -0400

    project setup stuffs

--- start.sh
+++ start.sh
@@ -9,6 +9,13 @@ kibana
 )
 
 oc new-project eparis-logging
+
+oc annotate namespace eparis-logging 'quota.openshift.io/cluster-resource-override-enabled=false'
+
+for quota in $(oc get quota --no-headers | cut -f 1 -d ' '); do
+  oc delete quota "${quota}"
+done
+
 for dir in "${dirs[@]}"; do
   pushd "${dir}" > /dev/null
     files=($(ls))
