bdaca6df237b49926a9cdbcbefa0e6eb13b70f1a
Eric Paris <eparis@redhat.com>
2017-10-16 10:33:31 -0400

    Start Script

--- start.sh
+++ start.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
+set -e
+
+dirs=(
+fluentd
+es
+kibana
+)
+
+oc new-project eparis-logging
+for dir in "${dirs[@]}"; do
+  pushd "${dir}" > /dev/null
+    files=($(ls))
+    for file in "${files[@]}"; do
+      oc create -f "${file}"
+    done
+  popd
+done
 oc adm policy add-scc-to-user hostmount-anyuid -z eparis-fluentd-es
 oc adm policy add-scc-to-user anyuid -z eparis-elasticsearch-logging
